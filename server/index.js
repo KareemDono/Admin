247116424ede72cbce4e1c5b2f099d66a2c2ef21
KareemDono <kareemjumaa16@gmail.com>
2023-05-30 20:16:41 +0300

    server

--- server/index.js
+++ server/index.js
@@ -10,11 +10,14 @@ const computerRoutes = require("./routes/computerRoute");
 const computerTypeRoutes = require("./routes/computerTypeRoute");
 const categoryRoutes = require("./routes/categoryRoute");
 
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const uri = "mongodb+srv://admin:admin@cluster0.edr434m.mongodb.net/Buildr?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.edr434m.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //users
